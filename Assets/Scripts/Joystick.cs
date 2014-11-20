4299f1c8132ceeef27e70b322c3a36ae3b80beba
syo-sa1982 <fujii1308@gmail.com>
2014-11-20 16:05:00 +0900

    ジョイスティックをC#に書き換え中

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Joystick : MonoBehaviour 
+{
+	class Boundary 
+	{
+		Vector2 min = Vector2.zero;
+		Vector2 max = Vector2.zero;
+	}
+
+	private int lastFingerId = -1;
+	private float tapTimeWindow;
+	private Vector2 fingerDownPos;
+	private float fingerDownTime;
+	private float firstDeltaTime = 0.5f;
+
+
+	private GUITexture gui;
+	private Rect defaultRect;
+	private Boundary guiBoundary = new Boundary();
+	private Vector2 guiTouchOffset;
+	private Vector2 guiCenter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
