f41f591059d4a4ccbb0d1952665331e46a78df29
syo-sa1982 <fujii1308@gmail.com>
2014-11-21 22:07:20 +0900

    ファイル追加

---
+++ Assets/Scripts/PlayerControllerMobile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerMobile : MonoBehaviour 
+{
+	public float speed = 3;
+	public float jumpPower = 6;
+
+	private Vector3 direction = Vector3.zero;
+	private CharacterController playerController;
+	private Joystick leftJoystick;
+	private Joystick abutton;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerController = GetComponent<CharacterController>();
+//		leftJoystick = GameObject.Find("LeftTouchPad").GetComponent( Joystick );
+//		abutton = GameObject.Find("AButton").GetComponent( Joystick );
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
