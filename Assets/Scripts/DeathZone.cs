d5ac4af51bdc7304b2d0e577c6142d6f2ba3b08f
syo-sa1982 <fujii1308@gmail.com>
2014-11-28 20:06:11 +0900

    スクリプトだけ commit

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider col) 
+	{
+		if(col.tag == "Player") {
+
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,14 @@ public class PlayerController : MonoBehaviour
 	public float jumpPower = 6;
 
 	private Vector3 direction = Vector3.zero;
-
 	private CharacterController playerController;
-
+	private Animator animetor;
 
 	// Use this for initialization
 	void Start () 
 	{
 		playerController = GetComponent<CharacterController>();
+		animetor = GetComponentInChildren<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -30,13 +30,16 @@ public class PlayerController : MonoBehaviour
 			if (inputDirection.magnitude > 0.1) {
 				transform.LookAt (transform.position + inputDirection);
 				direction += transform.forward * speed;
+				animetor.SetFloat ("Speed", direction.magnitude);
+			} else {
+				animetor.SetFloat ("Speed", 0);
 			}
 
 			if (Input.GetButton("Jump")) {
 				direction.y += jumpPower;
 			}
-
 		}
+
 		direction.y += Physics.gravity.y * Time.deltaTime;
 		playerController.Move (direction * Time.deltaTime);
 	}
