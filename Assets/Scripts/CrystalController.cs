9b9d057c40b5ece737b6752e9733452bcb2db8f3
syo-sa1982 <fujii1308@gmail.com>
2014-11-30 16:53:52 +0900

    Chapter3 に突入

---
+++ Assets/Scripts/CrystalController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalController : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider col) 
+	{
+		if(col.tag == "Player") {
+			col.gameObject.SendMessage ("AddCrystal");
+
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerCrystal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCrystal : MonoBehaviour 
+{
+	public int crystal;
+
+	public int Crystal { get; private set; }
+
+	public void AddCrystal ()
+	{
+		crystal++;
+	}
+
+}
