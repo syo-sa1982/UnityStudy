61f8bebbcbe56d7ce7036fff7517ff5e752e1549
syo-sa1982 <fujii1308@gmail.com>
2014-12-02 09:58:22 +0900

    $B%/%j%9%?%k$N%(%U%'%/%H$H$+(B

--- Assets/Scripts/CrystalController.cs
+++ Assets/Scripts/CrystalController.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class CrystalController : MonoBehaviour 
 {
+	public GameObject getEffect;
+
 	void OnTriggerEnter(Collider col) 
 	{
 		if(col.tag == "Player") {
 			col.gameObject.SendMessage ("AddCrystal");
 
 			Destroy (gameObject);
+
+			GameObject obj = GameObject.Instantiate (getEffect) as GameObject;
+			obj.transform.position = transform.position;
+
+			Destroy (obj, 1);
 		}
 	}
 }
--- Assets/Scripts/SimpleRotation.cs
+++ Assets/Scripts/SimpleRotation.cs
@@ -3,16 +3,12 @@ using System.Collections;
 
 public class SimpleRotation : MonoBehaviour 
 {
-
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		Vector3 v = transform.eulerAngles;
+		v.y = Mathf.Repeat( Time.time, 1 ) * 360f;
+		transform.eulerAngles = v;
 	}
 }
